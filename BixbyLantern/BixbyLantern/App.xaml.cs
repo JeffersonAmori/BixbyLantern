0a5835b0bfc9e497e7b26996317c3904607a4731
Jefferson Amori da Rocha <jefferson_adr@hotmail.com>
2019-10-28 21:46:36 -0300

    Add project files.

---
+++ BixbyLantern/BixbyLantern/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BixbyLantern
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override async void OnStart()
+        {
+            await Flashlight.TurnOnAsync();
+        }
+
+        protected override async void OnSleep()
+        {
+            await Flashlight.TurnOffAsync();
+        }
+
+        protected override async void OnResume()
+        {
+            await Flashlight.TurnOnAsync();
+        }
+    }
+}
